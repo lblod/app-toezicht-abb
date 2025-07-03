9726f23505d6bb2021ee4e4c76faabc086990e55
Kobe Mertens <kobe.mertens@hotmail.com>
2025-07-03 17:08:34 +0200

    add dispatcher config for site and address

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -44,7 +44,12 @@ defmodule Dispatcher do
   get "/files/*path", @json do
     Proxy.forward conn, path, "http://resource/files/"
   end
-
+  get "/adressen/*path", @json do
+    Proxy.forward conn, path, "http://cache/adressen/" # TODO change back to cache
+  end
+  get "/vestigingen/*path", @json do
+    Proxy.forward conn, path, "http://cache/vestigingen/"
+  end
   ###############################################################
   # Searching
   ###############################################################
